16a6a75630d5a4d1653d5bd65659a9cebf563a01
nicklorenzini <52935339+nick-lorenzini@users.noreply.github.com>
2021-02-21 19:40:25 -0800

    init

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: '#',
-  authDomain: '#',
-  databaseURL: '#',
-  projectId: '#',
-  storageBucket: '#',
-  messagingSenderId: '#',
-  appId: '#',
+  apiKey: "AIzaSyBTxCgHDqVQnXVC_hejrdFIPMxA70AS5lY",
+  authDomain: "a-auth-13856.firebaseapp.com",
+  databaseURL: "https://a-auth-13856-default-rtdb.firebaseio.com",
+  projectId: "a-auth-13856",
+  storageBucket: "a-auth-13856.appspot.com",
+  messagingSenderId: "34180598469",
+  appId: "1:34180598469:web:77de4442da9fb414aa7eac"
 };
